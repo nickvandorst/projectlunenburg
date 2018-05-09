fef8f655ad378a0ca5fcc52d502d6a87283d1d97
Rowan4 <39046689+Rowan4@users.noreply.github.com>
2018-05-09 11:16:06 +0200

    nieuwste versie

---
+++ src/AppBundle/Controller/ArtikelController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Klant;
+use AppBundle\Entity\KlantType;
+use AppBundle\Entity\Artikel;
+use AppBundle\Form\ArtikelType;
+//use Symfony\Component\HttpFoundation\Response;
+
+class ArtikelController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+ 	 * @Route("/alle/artikelen", name="alleartikelen")
+ 	 */
+ 	 public function alleArtikelen(Request $request) {
+
+ 	 	$artikelen = $this->getDoctrine()->getRepository("AppBundle:Artikel")->findAll();
+
+ 	  return new Response($this->render('alle_artikelen.html.twig', array('artikelen' => $artikelen)));
+ 	 }
+
+    // $artikelen = $this->getDoctrine()->getRepository("AppBundle:Artikel")->findAll();
+    // $tekst = "";
+    // return new Response($this->render('alle_artikelen.html.twig', array('artikelen' => $artikelen)));
+ 	// 	 }
+
+
+    /**
+     * @Route("/alle/klanten", name="alleklanten")
+     */
+     public function KlantOpVoornaam(Request $request) {
+      $klanten = $this->getDoctrine()->getRepository("AppBundle:Klant")->findAll();
+     	$tekst = "";
+     	return new Response($this->render('klanten.html.twig', array('klanten' => $klanten)));
+ 		 	}
+
+    /**
+     * @Route("/nieuw/artikel", name="nieuwartikel")
+     */
+    public function nieuwArtikel(Request $request) {
+        $nieuwArtikel = new Artikel();
+        $form = $this->createForm(ArtikelType::class, $nieuwArtikel);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($nieuwArtikel);
+            $em->flush();
+            return $this->redirect($this->generateUrl("alleartikelen"));
+        }
+
+        return new Response($this->render('form.html.twig', array('form' => $form->createView())));
+    }
+}
+?>
