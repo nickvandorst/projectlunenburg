e33cc1ac900d538d701e308f41543a67e339a6e2
Rowan4 <39046689+Rowan4@users.noreply.github.com>
2018-05-24 09:42:34 +0200

    commit

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -13,38 +13,45 @@ use AppBundle\Form\ArtikelInkoperType;
 class SearchController extends Controller {
 
   /**
-   * @Route("/inkoper/zoekomschrijving", name="inkoperzoekomschrijving")
-   */
-public function inkoperZoekomschrijving(Request $request) {
-  $em = $this->getDoctrine()->getManager();
-  $artikelen = $em->getRepository('AppBundle:Artikel')->findAll();
+     * @Route("/inkoper/zoekomschrijving", name="inkoperzoekomschrijving")
+     */
+  public function inkoperZoekomschrijving(Request $request) {
+                  $em = $this->getDoctrine()->getManager();
+                  $artikelen = $em->getRepository('AppBundle:Artikel');
 
-  if($request->isMethod('POST')) {
-      $omschrijving = $request->get('omschrijving');
-      $artikelen = $em->getRepository('AppBundle:Artikel')->findBy(array("omschrijving"=>$omschrijving));
-      }
+    if($request->isMethod('POST')) {
+        $omschrijving = $request->get('omschrijving');
+        $query = $em->createQuery(
+                     'SELECT a
+                      FROM AppBundle:Artikel a
+                      WHERE a.omschrijving LIKE :omschrijving'
+                      );
+                      $query->setParameter('omschrijving', '%' . $omschrijving . '%');
+                      $artikelen = $query->getResult();
+        }
 
-  return new Response($this->renderView("omschrijvingzoek.html.twig", array('artikelen'=>$artikelen)));
-  }
+    return new Response($this->renderView("omschrijvingzoek.html.twig", array('artikelen'=>$artikelen)));
+    }
 
-   /**
-   * @Route("/inkoper/zoekartikel", name="inkoperzoekartikel")
-   */
+
+  /**
+  * @Route("/inkoper/zoekartikel", name="inkoperzoekartikel")
+  */
 public function inkoperZoekartikel(Request $request) {
-  $em = $this->getDoctrine()->getManager();
-  $artikelen = $em->getRepository('AppBundle:Artikel');
-  $zoekwaarde = $request->request->get('artikelnummer');
-  if ($zoekwaarde == null) {
-    return new Response ("Het zoekveld is leeg!");
-  }
-  else {
-      $request->isMethod('POST'); 
-      $artikelnummer = $request->get('artikelnummer');
-      $artikelen = $em->getRepository('AppBundle:Artikel')->findBy(array("artikelnummer"=>$artikelnummer));
-      }
+ $em = $this->getDoctrine()->getManager();
+ $artikelen = $em->getRepository('AppBundle:Artikel');
+ $zoekwaarde = $request->request->get('artikelnummer');
+ if ($zoekwaarde == null) {
+   return new Response ("Het zoekveld is leeg!");
+ }
+ else {
+     $request->isMethod('POST');
+     $artikelnummer = $request->get('artikelnummer');
+     $artikelen = $em->getRepository('AppBundle:Artikel')->findBy(array("artikelnummer"=>$artikelnummer));
+     }
 
-  return new Response($this->renderView("zoek.html.twig", array('artikelen'=>$artikelen)));
-  }
+ return new Response($this->renderView("zoek.html.twig", array('artikelen'=>$artikelen)));
+ }
 
 }
 
