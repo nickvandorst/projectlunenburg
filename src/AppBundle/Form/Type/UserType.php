41312f7210a13449be8e8807d6279331c18233c1
markavans <39046481+markavans@users.noreply.github.com>
2018-06-05 11:01:08 +0200

    Login uit database + doorlogen werkt, nog niet af

---
+++ src/AppBundle/Form/Type/UserType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marice
+ * Date: 26-05-18
+ * Time: 13:59
+ */
+
+// src/AppBundle/Form/Type/UserType.php
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('username', TextType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password')
+            ))
+            ->add('roles', ChoiceType::class, [
+                'multiple' => true,
+                'expanded' => true, // render check-boxes
+                'choices' => [
+                     'Administrator' => 'ROLE_ADMINISTRATOR',
+                     'User' => 'ROLE_USER',
+                     'Inkoper' => 'ROLE_INKOPER',
+                     'Magazijnmeester' => 'ROLE_MAGAZIJNMEESTER',
+                    ],
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+}
